801288b2a0dc9e5490ad45ad03e096bf7cf93f09
Phutthiphong1996 <james.jirawuttinan@gmail.com>
2022-07-11 16:22:37 +0700

    git ignore

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const cors = require('cors')
+const bodyParser = require("body-parser");
+const msql = require("mysql");
+const port = 8080;
 
-app.get('/', (req, res) => {
-  res.json({msg: 'Hello World!' })
-})
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// const dbConnect = msql.createConnection({
+//   host: "",
+//   user: "",
+//   password: "",
+//   database: "",
+// });
+
+// dbConnect.connect()
+
+app.get("/getData", (req, res) => {
+  res.send("jame");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
